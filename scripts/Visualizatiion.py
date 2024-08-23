f1b4893621e23919b985c6aecbfa51c064077a7e
Marta233 <martaassefa23@gmail.com>
2024-08-23 19:55:01 +0300

    basics

---
+++ scripts/Visualizatiion.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objs as go
+from sklearn.linear_model import LinearRegression
+import numpy as np
+from scipy.stats import ttest_ind  # For statistical comparison
+
+class Visualization1:
+    def __init__(self, df):
+        self.df = df
+
+    def plot_line_graph(self, x, y):
+        self.df[x] = pd.to_datetime(self.df[x], format='mixed', dayfirst=True, errors='coerce')
+        fig = px.line(self.df, x=x, y=y)
+        fig.show()
