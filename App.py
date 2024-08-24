dbc19131b70e1d2d35f63d702ef6fa24101bec22
Marta233 <martaassefa23@gmail.com>
2024-08-24 04:42:05 +0300

    streamlit

--- App.py
+++ App.py
@@ -1,22 +1,15 @@
 import pandas as pd
 import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objs as go
-from sklearn.linear_model import LinearRegression
-import pandas as pd
-import plotly.express as px
-from plotly.subplots import make_subplots
+import streamlit as st
 import plotly.graph_objs as go
 from sklearn.linear_model import LinearRegression
 import numpy as np
 from scipy.stats import ttest_ind
 import matplotlib.pyplot as plt
 import seaborn as sns
-import streamlit as st
 class Visual:
     def __init__(self, df):
         self.df = df
-
     def plot_line_graph(self, x, y):
         # Convert x to datetime if necessary
         self.df[x] = pd.to_datetime(self.df[x], format='mixed', dayfirst=True, errors='coerce')
